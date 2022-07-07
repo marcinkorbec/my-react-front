36461fe806129681ddb38fd74895fd530109060c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-07-07 21:42:57 +0200

    Fixed error loader was not displayed.

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Loader.css"
 
-export const Loader = ()=> {
-	<div className="loader"/>
-}
+export const Loader = ()=> (
+	<div className="loader">
+		<div className="lds-ellipsis">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	</div>
+)
+
+
